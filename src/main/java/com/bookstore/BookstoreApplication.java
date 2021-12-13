3911d93c4132115014029c85fa0f23ae99c9bf58
MaarBy <jusz55@gmail.com>
2021-12-13 11:15:34 +0100

    back - security

--- src/main/java/com/bookstore/BookstoreApplication.java
+++ src/main/java/com/bookstore/BookstoreApplication.java
@@ -3,11 +3,14 @@ package com.bookstore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.bookstore.domain.User;
+
 @SpringBootApplication
 public class BookstoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
+		
 	}
 
 }
